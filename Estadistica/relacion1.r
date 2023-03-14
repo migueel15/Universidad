4a6d941507da30c6479a87a160bf2a8e46dddb2e
Miguel Dorado <miguelangeldorado10@gmail.com>
2023-03-15 00:06:07 +0100

    Add estadistica

---
+++ Estadistica/relacion1.r
@@ -0,0 +1,5 @@
+# Ejercicio 1
+# a) Población: Todos los componentes que se estudian
+# Individuos: Cada componente estudiado.
+# b) Caracter: Durabilidad, tipo: cuantitativo, modalidad: continua(años de duracion)
+# c) Cuanto mayor sea la media de las vida util de cada componente mayor será la vida útil del pc.
