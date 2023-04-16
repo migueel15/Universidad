54df9f463c7f1f1812ca1e3f906388e0000ce029
migueel15 <miguelangeldorado10@gmail.com>
2023-04-16 22:56:04 +0200

    Update practice directory

--- POO/GUI/src/Modelo.java
+++ POO/GUI/src/Modelo.java
@@ -1,14 +1,15 @@
 public class Modelo {
   private double total;
-  public Modelo(){
+
+  public Modelo() {
     this.total = 0;
   }
 
-  public double getTotal(){
+  public double getTotal() {
     return total;
   }
 
-  public void incTotal(double valor){
+  public void incTotal(double valor) {
     total += valor;
   }
 }
--- POO/GUI/src/Vista.java
+++ POO/GUI/src/Vista.java
@@ -2,8 +2,12 @@ import java.awt.event.ActionListener;
 
 public interface Vista {
   public static String SUMAR = "Sumar";
+
   public void registrarControlador(ActionListener c);
+
   public double getValor();
+
   public void setTotal(double valor);
+
   public void mensaje(String m);
 }
