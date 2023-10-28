ea0b4d450ad543fe391443d25df95abb530e9ec2
migueel15 <miguelangeldorado10@gmail.com>
2023-10-28 20:53:15 +0200

    Parte 3 terminada

--- segundo/ADA/practica3/src/ejercicios/ArraysConRepeticiones.java
+++ segundo/ADA/practica3/src/ejercicios/ArraysConRepeticiones.java
@@ -1,5 +1,5 @@
 package ejercicios;
-/*
+
 public class ArraysConRepeticiones {
 
 	//Precondición: hay un elemento repetido=> v.length >= 2
@@ -8,7 +8,27 @@ public class ArraysConRepeticiones {
 	}
 	
 	private static int encuentraElem(int [] v, int izq, int der) {
-		//Completar la implementación del método
+    int pos;
+    if(izq != der) {
+      pos = (izq+der-1)/2;
+    }else{
+      pos = izq;
+    }
+    // si está en su sitio buscamos a la derecha
+    if(pos == v[pos]){
+      return encuentraElem(v,pos+1,der);
+    }else{ // si no está en su sitio el duplicado está a la izquierda
+      if(pos >= 1 && v[pos] == v[pos-1]){
+        return v[pos];
+      }else{
+        return encuentraElem(v,izq,pos);
+      }
+    }
 	}
+
+  public static void main(String[] args) {
+    int[] lista = {0,0};
+    int valor = encuentraElem(lista);
+    System.out.println(valor);
+  }
 }
-*/
