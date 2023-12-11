5fc9a22f975092daf632f5e8f98686185196d896
migueel15 <miguelangeldorado10@gmail.com>
2023-12-11 22:13:01 +0100

    FIX: Edit order

--- segundo/ADA/practicas/practica6/src/ColaPrioridad.java
+++ segundo/ADA/practicas/practica6/src/ColaPrioridad.java
@@ -1,35 +1,48 @@
 import java.util.Iterator;
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.PriorityQueue;
 
 public class ColaPrioridad {
-	private SortedSet<Estado> cola;
+	private PriorityQueue<Estado> cola;
 	
 	public ColaPrioridad() {
-		cola = new TreeSet<Estado>();
+		cola = new PriorityQueue<Estado>();
 	}
-	
+	/**
+	 * Indica si la cola no tiene elementos
+	 */
 	public boolean estaVacia() {
 		return cola.size() == 0;
 	}
 	
+	/**
+	 * Inserta el estado dado en la cola de prioridad
+	 */
 	public void insertar(Estado e) {
 		cola.add(e);
 	}
 	
+	/**
+	 * Extrae la cabeza de la cola de prioridad
+	 */
 	public Estado extraer() {
 		Estado res = null;
 		if (cola.size()>0) {
-			res = cola.first();
-			cola.remove(res);
+			res = cola.poll();
 		}
 		return res;
 	}
 	
+	/**
+	 * Elimina el estado dado de la cola de prioridad
+	 */
 	public void eliminar(Estado e) {
 		cola.remove(e);
 	}
 	
+	/**
+	 * Elimina todos los estados de la cola cuyo valor de cota no sea menor
+	 * que el valor dado por parámetro
+	 */
 	public void eliminar(int valorCota) {
 		Iterator<Estado> it = cola.iterator();
 		while (it.hasNext()) {
