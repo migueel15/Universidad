8999bcd77df35d1fb00448745e38855282cf5b21
migueel15 <miguelangeldorado10@gmail.com>
2024-01-10 21:14:57 +0100

    add parte haskell

--- segundo/ED/examenes/septiembre2020/haskell/AVL.hs
+++ segundo/ED/examenes/septiembre2020/haskell/AVL.hs
@@ -4,57 +4,58 @@
  - Student's group:
  -----------------------------------------------------------------------------}
 
-module AVL 
-  ( 
-    Weight
-  , Capacity
-  , AVL (..)
-  , Bin
-  , emptyBin
-  , remainingCapacity
-  , addObject
-  , maxRemainingCapacity
-  , height
-  , nodeWithHeight
-  , node
-  , rotateLeft
-  , addNewBin
-  , addFirst
-  , addAll
-  , toList
-  , linearBinPacking
-  , seqToList
-  , addAllFold
-  ) where
+module AVL
+  ( Weight,
+    Capacity,
+    AVL (..),
+    Bin,
+    emptyBin,
+    remainingCapacity,
+    addObject,
+    maxRemainingCapacity,
+    height,
+    nodeWithHeight,
+    node,
+    rotateLeft,
+    addNewBin,
+    addFirst,
+    addAll,
+    toList,
+    linearBinPacking,
+    seqToList,
+    addAllFold,
+  )
+where
 
 type Capacity = Int
-type Weight= Int
 
-data Bin = B Capacity [Weight] 
+type Weight = Int
 
-data AVL = Empty | Node Bin Int Capacity AVL AVL deriving Show
+data Bin = B Capacity [Weight]
 
+data AVL = Empty | Node Bin Int Capacity AVL AVL deriving (Show)
 
 emptyBin :: Capacity -> Bin
-emptyBin _ = undefined
+emptyBin c = B c []
 
 remainingCapacity :: Bin -> Capacity
-remainingCapacity _ = undefined
+remainingCapacity (B c lista) = c
 
 addObject :: Weight -> Bin -> Bin
-addObject _ _ = undefined
+addObject peso (B c lista)
+  | peso > c = error "peso mayor a cantidad restante"
+  | otherwise = B (c - peso) (peso : lista)
 
 maxRemainingCapacity :: AVL -> Capacity
-maxRemainingCapacity _ = undefined
+maxRemainingCapacity (Node bin h cap nl nr) = cap
 
 height :: AVL -> Int
-height _ = undefined
+height (Node bin h cap nl nr) = h
 
-
- 
 nodeWithHeight :: Bin -> Int -> AVL -> AVL -> AVL
-nodeWithHeight _ _ _ _ = undefined
-
+nodeWithHeight bin@(B c lista) altura nl nr = Node bin altura (maxcap) nl nr
+  where
+    maxcap = max (maxRemainingCapacity nl) (maxRemainingCapacity nr)
 
 node :: Bin -> AVL -> AVL -> AVL
 node _ _ _ = undefined
@@ -64,11 +65,11 @@ rotateLeft _ _ _ = undefined
 
 addNewBin :: Bin -> AVL -> AVL
 addNewBin _ _ = undefined
- 
+
 addFirst :: Capacity -> Weight -> AVL -> AVL
 addFirst _ _ _ = undefined
 
-addAll:: Capacity -> [Weight] -> AVL
+addAll :: Capacity -> [Weight] -> AVL
 addAll _ _ = undefined
 
 toList :: AVL -> [Bin]
@@ -79,24 +80,21 @@ toList _ = undefined
   ONLY FOR STUDENTS WITHOUT CONTINUOUS ASSESSMENT
  -}
 
-data Sequence = SEmpty | SNode Bin Sequence deriving Show  
+data Sequence = SEmpty | SNode Bin Sequence deriving (Show)
 
-linearBinPacking:: Capacity -> [Weight] -> Sequence
+linearBinPacking :: Capacity -> [Weight] -> Sequence
 linearBinPacking _ _ = undefined
 
-seqToList:: Sequence -> [Bin]
+seqToList :: Sequence -> [Bin]
 seqToList _ = undefined
 
-addAllFold:: [Weight] -> Capacity -> AVL 
+addAllFold :: [Weight] -> Capacity -> AVL
 addAllFold _ _ = undefined
-
-
 
 {- No modificar. Do not edit -}
 
 objects :: Bin -> [Weight]
 objects (B _ os) = reverse os
 
-  
 instance Show Bin where
-  show b@(B c os) = "Bin("++show c++","++show (objects b)++")"
+  show b@(B c os) = "Bin(" ++ show c ++ "," ++ show (objects b) ++ ")"
