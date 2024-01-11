f8d70d6164a6c9ee03cb7349568d391d86e398cb
migueel15 <miguelangeldorado10@gmail.com>
2024-01-11 19:03:58 +0100

    add: terminada parte haskell septiembre2020

--- segundo/ED/examenes/septiembre2020/haskell/AVL.hs
+++ segundo/ED/examenes/septiembre2020/haskell/AVL.hs
@@ -47,33 +47,50 @@ addObject peso (B c lista)
   | otherwise = B (c - peso) (peso : lista)
 
 maxRemainingCapacity :: AVL -> Capacity
+maxRemainingCapacity Empty = 0
 maxRemainingCapacity (Node bin h cap nl nr) = cap
 
 height :: AVL -> Int
+height Empty = 0
 height (Node bin h cap nl nr) = h
 
 nodeWithHeight :: Bin -> Int -> AVL -> AVL -> AVL
 nodeWithHeight bin@(B c lista) altura nl nr = Node bin altura (maxcap) nl nr
   where
-    maxcap = max (maxRemainingCapacity nl) (maxRemainingCapacity nr)
+    maxcap = max (max (maxRemainingCapacity nl) (maxRemainingCapacity nr)) c
 
 node :: Bin -> AVL -> AVL -> AVL
-node _ _ _ = undefined
+node bin@(B c lista) nl nr = Node bin getaltura getcapacidad nl nr
+  where
+    getaltura = max (height nl) (height nr) + 1
+    getcapacidad = max (max (maxRemainingCapacity nl) (maxRemainingCapacity nr)) c
 
 rotateLeft :: Bin -> AVL -> AVL -> AVL
-rotateLeft _ _ _ = undefined
+rotateLeft c@(B cc listac) l@(Node bl hl cl nll nrl) (Node bx hx cx r1 r2) = raiz
+  where
+    nodoizq = node c l r1
+    raiz = node bx nodoizq r2
 
 addNewBin :: Bin -> AVL -> AVL
-addNewBin _ _ = undefined
+addNewBin bin@(B c lista) Empty = Node bin c 1 Empty Empty
+addNewBin bin@(B bc lista) (Node x@(B xc listac) h c nl nr)
+  | height nr - height nl > 1 = rotateLeft x nl (addNewBin bin nr)
+  | otherwise = Node x h c nl (addNewBin bin nr)
 
 addFirst :: Capacity -> Weight -> AVL -> AVL
-addFirst _ _ _ = undefined
+addFirst cap wg Empty = addNewBin (B (cap - wg) [wg]) Empty
+addFirst cap wg avl@(Node bin h c nl nr)
+  | c < wg = addNewBin (B (cap - wg) [wg]) avl
+  | maxRemainingCapacity nl >= wg = Node bin h c (addFirst cap wg nl) nr
+  | maxRemainingCapacity avl >= wg = Node (addObject wg bin) h (c - wg) nl nr
+  | otherwise = Node bin h c nl (addFirst cap wg nr)
 
 addAll :: Capacity -> [Weight] -> AVL
-addAll _ _ = undefined
+addAll cap lista = foldr (\wg ini -> addFirst cap wg ini) Empty lista
 
 toList :: AVL -> [Bin]
-toList _ = undefined
+toList Empty = []
+toList (Node bin h c nl nr) = toList nl ++ [bin] ++ toList nr
 
 {-
 	SOLO PARA ALUMNOS SIN EVALUACION CONTINUA
