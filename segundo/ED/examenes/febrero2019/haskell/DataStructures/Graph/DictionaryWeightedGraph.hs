70d37944198558030c104c0b855bd6916c5c4ca3
migueel15 <miguelangeldorado10@gmail.com>
2024-01-14 19:34:37 +0100

    add: parte de haskell febrero2019

--- segundo/ED/examenes/febrero2019/haskell/DataStructures/Graph/DictionaryWeightedGraph.hs
+++ segundo/ED/examenes/febrero2019/haskell/DataStructures/Graph/DictionaryWeightedGraph.hs
@@ -5,61 +5,85 @@
 --
 -- Examen 4 de febrero de 2019
 --
--- ALUMNO/NAME:
--- GRADO/STUDIES:
--- NÚM. MÁQUINA/MACHINE NUMBER:
+-- ALUMNO/NAME: Miguel Angel Dorado Maldonado
+-- GRADO/STUDIES: Software A
+-- NÚM. MÁQUINA/MACHINE NUMBER: xxx
 --
 -- Weighted Graph implemented by using a dictionary from
 -- sources to another dictionary from destinations to weights
 ----------------------------------------------
 
 module DataStructures.Graph.DictionaryWeightedGraph
-  ( WeightedGraph
-  , WeightedEdge(WE)
-  , empty
-  , isEmpty
-  , mkWeightedGraphEdges
-  , addVertex
-  , addEdge
-  , vertices
-  , numVertices
-  , edges
-  , numEdges
-  , successors
-  ) where
+  ( WeightedGraph,
+    WeightedEdge (WE),
+    empty,
+    isEmpty,
+    mkWeightedGraphEdges,
+    addVertex,
+    addEdge,
+    vertices,
+    numVertices,
+    edges,
+    numEdges,
+    successors,
+  )
+where
 
-import Data.List(nub, intercalate)
+import Data.List (intercalate, nub)
+import DataStructures.Dictionary.AVLDictionary qualified as D
 
-import qualified DataStructures.Dictionary.AVLDictionary as D
-
-data WeightedEdge a w  = WE a w a deriving Show
+data WeightedEdge a w = WE a w a deriving (Show)
 
 instance (Eq a, Eq w) => Eq (WeightedEdge a w) where
-  WE u w v == WE u' w' v' = (u==u' && v==v' || u==v' && v==u')
-                              && w == w'
+  WE u w v == WE u' w' v' =
+    (u == u' && v == v' || u == v' && v == u')
+      && w == w'
 
 instance (Eq a, Ord w) => Ord (WeightedEdge a w) where
   compare (WE _ w _) (WE _ w' _) = compare w w'
 
-data WeightedGraph a w  = WG (D.Dictionary a (D.Dictionary a w))
+data WeightedGraph a w = WG (D.Dictionary a (D.Dictionary a w))
 
 empty :: WeightedGraph a w
 empty = WG D.empty
 
 addVertex :: (Ord a) => WeightedGraph a w -> a -> WeightedGraph a w
-addVertex = undefined
+addVertex (WG dict) key = WG (D.insert key D.empty dict)
 
 addEdge :: (Ord a, Show a) => WeightedGraph a w -> a -> a -> w -> WeightedGraph a w
-addEdge = undefined
+addEdge (WG dict) src dst wg
+  | D.isDefinedAt src dict = WG (D.insert src (D.insert dst wg (toDict (D.valueOf src dict))) dict)
+  | otherwise = error "No existe el vertice"
+
+toDict :: Maybe a -> a
+toDict (Just a) = a
 
 edges :: (Eq a, Eq w) => WeightedGraph a w -> [WeightedEdge a w]
-edges  = undefined
+edges (WG dic) = [a | b <- aux (D.keys dic) (D.values dic), a <- b]
+  where
+    aux [] [] = []
+    aux [x] [y] = [aux' x (D.keys y) (D.values y)]
+    aux (x : xs) (y : ys) = aux' x (D.keys y) (D.values y) : aux xs ys
 
-successors :: (Ord a, Show a) => WeightedGraph a w -> a -> [(a,w)]
-successors  = undefined
+    aux' x [] [] = []
+    aux' x [y] [z] = [WE x z y]
+    aux' x (y : ys) (z : zs) = WE x z y : aux' x ys zs
 
+successors :: (Ord a, Show a) => WeightedGraph a w -> a -> [(a, w)]
+successors (WG dict) v
+  | D.isDefinedAt v dict = aux (D.keys dict) (D.values dict) v
+  where
+    aux [x] [y] a
+      | a /= x = []
+      | otherwise = aux' (D.keys y) (D.values y)
+    aux (x : xs) (y : ys) a
+      | a /= x = aux xs ys a
+      | otherwise = aux' (D.keys y) (D.values y)
+    aux' [x] [] = []
+    aux' [x] [y] = [(x, y)]
+    aux' (x : xs) (y : ys) = (x, y) : aux' xs ys
 
--- NO EDITAR A PARTIR DE AQUÍ    
+-- NO EDITAR A PARTIR DE AQUÍ
 -- DON'T EDIT ANYTHING BELOW THIS COMMENT
 
 vertices :: WeightedGraph a w -> [a]
@@ -81,8 +105,8 @@ numEdges :: (Eq a, Eq w) => WeightedGraph a w -> Int
 numEdges = length . edges
 
 instance (Eq a, Show a, Eq w, Show w) => Show (WeightedGraph a w) where
-  show wg  = "DictionaryWeightedGraph("++vs++", "++as++")"
-   where
-    vs  = "("++ intercalate ", " (map show (vertices wg)) ++")"
-    as  = "(" ++ intercalate ", " (map showEdge (edges wg)) ++ ")"
-    showEdge (WE x w y)  = intercalate "-" [ show x, show w, show y ]
+  show wg = "DictionaryWeightedGraph(" ++ vs ++ ", " ++ as ++ ")"
+    where
+      vs = "(" ++ intercalate ", " (map show (vertices wg)) ++ ")"
+      as = "(" ++ intercalate ", " (map showEdge (edges wg)) ++ ")"
+      showEdge (WE x w y) = intercalate "-" [show x, show w, show y]
--- segundo/ED/examenes/febrero2019/haskell/Kruskal.hs
+++ segundo/ED/examenes/febrero2019/haskell/Kruskal.hs
@@ -17,8 +17,46 @@ import DataStructures.Dictionary.AVLDictionary qualified as D
 import DataStructures.Graph.DictionaryWeightedGraph
 import DataStructures.PriorityQueue.LinearPriorityQueue qualified as Q
 
+fromJust :: Maybe a -> a
+fromJust (Just a) = a
+
+mkPQueue :: (Ord a) => [a] -> Q.PQueue a
+mkPQueue xs = foldr Q.enqueue Q.empty xs
+
+llenar xs = mkPQueue xs
+
 kruskal :: (Ord a, Ord w) => WeightedGraph a w -> [WeightedEdge a w]
-kruskal = undefined
+kruskal g = aux cola first (dicIni (vertices g))
+  where
+    cola = llenar (edges g)
+
+    dicIni :: (Ord a) => [a] -> D.Dictionary a a
+    dicIni [x] = D.insert x x (D.empty)
+    dicIni (x : xs) = D.insert x x (dicIni xs)
+
+    first = Q.first cola
+    cola' = Q.dequeue cola
+
+    representante :: (Ord a) => a -> D.Dictionary a a -> a
+    representante x dic
+      | x /= fromJust (D.valueOf x dic) = representante (fromJust (D.valueOf x dic)) dic
+      | otherwise = x
+
+    src :: (Ord a, Ord w) => WeightedEdge a w -> a
+    src (WE a w b) = a
+    dst :: (Ord a, Ord w) => WeightedEdge a w -> a
+    dst (WE a w b) = b
+
+    aux :: (Ord a, Ord w) => Q.PQueue (WeightedEdge a w) -> WeightedEdge a w -> D.Dictionary a a -> [WeightedEdge a w]
+    aux cola primeroCola dicVertices
+      | Q.isEmpty cola && representante (src primeroCola) dicVertices /= representante (dst primeroCola) dicVertices = [primeroCola]
+      | Q.isEmpty cola && representante (src primeroCola) dicVertices == representante (dst primeroCola) dicVertices = []
+      | not (Q.isEmpty cola) && representante (src primeroCola) dicVertices /= representante (dst primeroCola) dicVertices = primeroCola : aux cola'' first' (D.insert (dst primeroCola) (src primeroCola) dicVertices)
+      | not (Q.isEmpty cola) && representante (src primeroCola) dicVertices == representante (dst primeroCola) dicVertices = aux cola'' first' dicVertices
+      | otherwise = aux cola'' first' dicVertices
+      where
+        first' = Q.first cola
+        cola'' = Q.dequeue cola
 
 -- Solo para evaluación continua / only for part time students
 kruskals :: (Ord a, Ord w) => WeightedGraph a w -> [[WeightedEdge a w]]
