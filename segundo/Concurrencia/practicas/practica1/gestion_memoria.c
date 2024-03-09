437c66fb275032d5f0a9468eb5a7db598768ff5f
migueel15 <miguelangeldorado10@gmail.com>
2024-03-09 15:57:49 +0100

    add practica 1 terminada

--- segundo/Concurrencia/practicas/practica1/gestion_memoria.c
+++ segundo/Concurrencia/practicas/practica1/gestion_memoria.c
@@ -55,22 +55,73 @@ void obtener(T_Manejador *manejador, unsigned tam, unsigned *dir,
 void mostrar(T_Manejador manejador) {
   T_Manejador ptr = manejador;
   while (ptr != NULL) {
+    printf("--------------------\n");
     printf("Desde %d a %d: Libre\n", ptr->inicio, ptr->fin);
     ptr = ptr->sig;
   }
 }
 
+T_Manejador crearNodo(unsigned ini, unsigned tam, T_Manejador sig) {
+  T_Manejador nodo = malloc(sizeof(struct T_Nodo));
+  nodo->inicio = ini;
+  nodo->fin = ini + tam - 1;
+  nodo->sig = sig;
+  return nodo;
+}
+
 void devolver(T_Manejador *manejador, unsigned tam, unsigned dir) {
   T_Manejador ant = NULL;
   T_Manejador ptr = *manejador;
+  int encontrado = 0;
 
-  while (ptr != NULL) {
-    if (dir <= ptr->inicio) {
-      // añadir aqui
-      T_Manejador p_nodo = malloc(sizeof(struct T_Nodo));
+  // Buscamos los nodos que nos interesa
+  while (ptr != NULL && !encontrado) {
+    if (dir >= ptr->inicio) {
+      // avanzar
+      ant = ptr;
+      ptr = ptr->sig;
+    } else {
+      encontrado = 1;
+    }
+  }
+
+  if (ant == NULL) {
+    // SE INSERTA AL PRINCIPIO
+    if (dir + tam < ptr->inicio) {
+      // se crea un nodo
+      T_Manejador nodo = crearNodo(dir, tam, ptr);
+      *manejador = nodo;
 
     } else {
-      // rotar
+      // se aumenta el inicio de ptr
+      ptr->inicio = dir;
+    }
+  } else if (ptr == NULL) {
+    // SE INSERTA AL FINAL
+    if (dir - 1 != ant->fin) {
+      // crear nuevo nodo
+      T_Manejador nodo = crearNodo(dir, tam, NULL);
+      ant->sig = nodo;
+    } else {
+      // aumentar ultimo nodo
+      ant->fin += tam;
+    }
+  } else {
+    // SE INSERTA ENTRE NODOS
+    if (ant->fin + 1 == dir && ptr->inicio != (dir + tam)) {
+      // ampliar anterior
+      ant->fin = ant->fin + tam - 1;
+    } else if (ant->fin != dir) {
+      // crear un nuevo nodo
+      T_Manejador nuevo = crearNodo(dir, tam, ptr);
+      if (nuevo != NULL) {
+        ant->sig = nuevo;
+      }
+    } else {
+      // ocupa todo el hueco de tiene que unir
+      ant->fin = ptr->fin;
+      ant->sig = ptr->sig;
+      free(ptr);
     }
   }
 }
