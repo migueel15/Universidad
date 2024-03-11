eefb524dabfc095371826d08fd351ce137f2c316
migueel15 <miguelangeldorado10@gmail.com>
2024-03-11 13:31:04 +0100

    add backup3

--- segundo/SO/backup3.sh
+++ segundo/SO/backup3.sh
@@ -6,20 +6,11 @@ if [[ ! -d $BACKUP_PATH ]]; then
   mkdir $BACKUP_PATH
 fi
 
+if [[ ! -d $BACKUP_PATH/$CURRENT_DATE ]]; then
+  mkdir $BACKUP_PATH/$CURRENT_DATE
+fi
+
 for current in $@
 do
-  if [[ ! -d $BACKUP_PATH/$CURRENT_DATE ]]; then
-    mkdir $BACKUP_PATH/$CURRENT_DATE
-  # else
-  #   A=$(ls $current* | wc -w)
-  #   if [[ $A -ge 9 ]]; then
-  #     echo "Se ha superado el número máximo de versiones"
-  #   else
-  #     date_str=$(date "+%y%m%d")
-  #     Version="${date_str}_$current"
-  #     cp $current $Version
-  #   fi
-  fi
+      cp $current $BACKUP_PATH/$CURRENT_DATE/
 done
-
-
