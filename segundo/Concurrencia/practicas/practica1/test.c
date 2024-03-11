77a99a48162c30621589ebe418bd8465597b3723
migueel15 <miguelangeldorado10@gmail.com>
2024-03-11 18:22:44 +0100

    Update Universidad: lun 11 mar 2024 18:22:44 CET

---
+++ segundo/Concurrencia/practicas/practica1/test.c
@@ -0,0 +1,29 @@
+
+#include "gestion_memoria.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+  T_Manejador manej;
+  unsigned ok;
+  unsigned dir;
+
+  crear(&manej);
+  mostrar(manej);
+
+  obtener(&manej, 500, &dir,
+          &ok); /* Se ha hecho una foto. Se necesita memoria */
+  if (ok) {
+    mostrar(manej);
+    printf("la direccion de comienzo es: %d\n", dir);
+  } else {
+    printf("No es posible obtener esa memoria\n");
+  }
+
+  devolver(&manej, 200,
+           0); /* Se ha enviado parte de la foto. Ya se puede borrar */
+  mostrar(manej);
+  devolver(&manej, 150, 200);
+  mostrar(manej);
+}
