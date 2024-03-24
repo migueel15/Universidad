d7519434545ac64cab09cf6273bd39e24746fc49
migueel15 <miguelangeldorado10@gmail.com>
2024-03-24 20:57:34 +0100

    add metodos arbol

--- segundo/Concurrencia/practicas/practica2/arbolbb.c
+++ segundo/Concurrencia/practicas/practica2/arbolbb.c
@@ -17,3 +17,30 @@ void Destruir(T_Arbol *arbol) {
     }
   }
 }
+void Insertar(T_Arbol *arbol, unsigned num) {
+  if ((*arbol) == NULL) {
+    T_Arbol nodo = malloc(sizeof(struct T_Nodo));
+    nodo->dato = num;
+    nodo->izq = NULL;
+    nodo->der = NULL;
+    *arbol = nodo;
+  } else if ((*arbol)->dato > num) {
+    Insertar(&(*arbol)->izq, num);
+  } else {
+    Insertar(&(*arbol)->der, num);
+  }
+}
+void Mostrar(T_Arbol arbol) {
+  if (arbol != NULL) {
+    Mostrar(arbol->izq);
+    printf("%d ", arbol->dato);
+    Mostrar(arbol->der);
+  }
+}
+void Salvar(T_Arbol arbol, FILE *fichero) {
+  if (arbol != NULL) {
+    Salvar(arbol->izq, fichero);
+    fwrite(&(arbol->dato), sizeof(unsigned int), 1, fichero);
+    Salvar(arbol->der, fichero);
+  }
+}
