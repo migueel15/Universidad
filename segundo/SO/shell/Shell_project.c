0736c12cb167860975f31571aa59f04d1a67311b
migueel15 <miguelangeldorado10@gmail.com>
2024-04-08 01:54:38 +0200

    Update Universidad: lun 08 abr 2024 01:54:38 CEST

--- segundo/SO/shell/Shell_project.c
+++ segundo/SO/shell/Shell_project.c
@@ -9,45 +9,46 @@ Some code adapted from "Fundamentos de Sistemas Operativos", Silberschatz et al.
 
 To compile and run the program:
    $ gcc Shell_project.c job_control.c -o Shell
-   $ ./Shell          
-	(then type ^D to exit program)
+   $ ./Shell
+        (then type ^D to exit program)
 
 **/
 
-#include "job_control.h"   // remember to compile with module job_control.c 
+#include "job_control.h" // remember to compile with module job_control.c
 
 #define MAX_LINE 256 /* 256 chars per line, per command, should be enough. */
 
 // -----------------------------------------------------------------------
-//                            MAIN          
+//                            MAIN
 // -----------------------------------------------------------------------
 
-int main(void)
-{
-	char inputBuffer[MAX_LINE]; /* buffer to hold the command entered */
-	int background;             /* equals 1 if a command is followed by '&' */
-	char *args[MAX_LINE/2];     /* command line (of 256) has max of 128 arguments */
-	// probably useful variables:
-	int pid_fork, pid_wait; /* pid for created and waited process */
-	int status;             /* status returned by wait */
-	enum status status_res; /* status processed by analyze_status() */
-	int info;				/* info processed by analyze_status() */
+int main(void) {
+  char inputBuffer[MAX_LINE]; /* buffer to hold the command entered */
+  int background;             /* equals 1 if a command is followed by '&' */
+  char *args[MAX_LINE / 2]; /* command line (of 256) has max of 128 arguments */
+  // probably useful variables:
+  int pid_fork, pid_wait; /* pid for created and waited process */
+  int status;             /* status returned by wait */
+  enum status status_res; /* status processed by analyze_status() */
+  int info;               /* info processed by analyze_status() */
 
-	while (1)   /* Program terminates normally inside get_command() after ^D is typed*/
-	{   		
-		printf("COMMAND->");
-		fflush(stdout);
-		get_command(inputBuffer, MAX_LINE, args, &background);  /* get next command */
-		
-		if(args[0]==NULL) continue;   // if empty command
+  /* Program terminates normally inside get_command() after ^D is typed*/
+  while (1) {
+    printf("COMMAND->");
+    fflush(stdout);
+    get_command(inputBuffer, MAX_LINE, args,
+                &background); /* get next command */
 
-		/* the steps are:
-			 (1) fork a child process using fork()
-			 (2) the child process will invoke execvp()
-			 (3) if background == 0, the parent will wait, otherwise continue 
-			 (4) Shell shows a status message for processed command 
-			 (5) loop returns to get_commnad() function
-		*/
+    if (args[0] == NULL)
+      continue; // if empty command
 
-	} // end while
+    /* the steps are:
+             (1) fork a child process using fork()
+             (2) the child process will invoke execvp()
+             (3) if background == 0, the parent will wait, otherwise continue
+             (4) Shell shows a status message for processed command
+             (5) loop returns to get_commnad() function
+    */
+
+  } // end while
 }
