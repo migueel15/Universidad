20aaf5c712bcdbb10c36ac1eeef09a9de5f7fad9
migueel15 <miguelangeldorado10@gmail.com>
2024-06-12 14:36:32 +0200

    Update Universidad: mié 12 jun 2024 14:36:32 CEST

--- segundo/SI/practicas/practica5/practica5.py
+++ segundo/SI/practicas/practica5/practica5.py
@@ -1,13 +1,12 @@
-import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import StratifiedKFold, GridSearchCV
-from sklearn.tree import DecisionTreeClassifier, plot_tree
+import numpy as np
+import pandas as pd
+from sklearn.metrics import (accuracy_score, f1_score, precision_score,
+                             roc_curve)
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import roc_curve, accuracy_score, f1_score, precision_score
-
-
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeClassifier, plot_tree
 
 dataset = pd.read_csv("./student-mat.csv", delimiter=";")
 
