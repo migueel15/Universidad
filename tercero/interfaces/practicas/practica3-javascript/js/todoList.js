2de86469a3ed102a91ed0b4b91940f82e581e6cf
migueel15 <miguelangeldorado10@gmail.com>
2025-03-13 17:06:52 +0100

    Update Universidad: jue 13 mar 2025 17:06:52 CET

--- tercero/interfaces/practicas/practica3-javascript/js/todoList.js
+++ tercero/interfaces/practicas/practica3-javascript/js/todoList.js
@@ -3,27 +3,30 @@ const inputField = document.getElementById('taskInput');
 
 const handleAddTask = () => {
 	const taskName = inputField.value;
-	const ul = document.getElementById('todoList');
-	const li = document.createElement('li');
-	li.className = 'list-group-item d-flex flex-row align-items-center';
-	const span = document.createElement('span');
-	span.className = 'taskName flex-grow-1';
-	span.innerHTML = taskName;
-	const imgUp = document.createElement('img');
-	imgUp.addEventListener('click', handleMoveTaskUp);
-	imgUp.src = 'images/taskUp.png';
-	const imgDown = document.createElement('img');
-	imgDown.addEventListener('click', handleMoveTaskDown);
-	imgDown.src = 'images/taskDown.png';
-	const imgRemove = document.createElement('img');
-	imgRemove.addEventListener('click', handleRemoveTask);
-	imgRemove.src = 'images/removeTask.png';
-	li.appendChild(span);
-	li.appendChild(imgUp);
-	li.appendChild(imgDown);
-	li.appendChild(imgRemove);
-	ul.appendChild(li);
-	inputField.value = '';
+	if (taskName !== '') {
+		const ul = document.getElementById('todoList');
+		const li = document.createElement('li');
+		li.className = 'list-group-item d-flex flex-row align-items-center';
+		const span = document.createElement('span');
+		span.className = 'taskName flex-grow-1';
+		span.innerHTML = taskName;
+		const imgUp = document.createElement('img');
+		imgUp.addEventListener('click', handleMoveTaskUp);
+		imgUp.src = 'images/taskUp.png';
+		const imgDown = document.createElement('img');
+		imgDown.addEventListener('click', handleMoveTaskDown);
+		imgDown.src = 'images/taskDown.png';
+		const imgRemove = document.createElement('img');
+		imgRemove.addEventListener('click', handleRemoveTask);
+		imgRemove.src = 'images/removeTask.png';
+		li.appendChild(span);
+		li.appendChild(imgUp);
+		li.appendChild(imgDown);
+		li.appendChild(imgRemove);
+		ul.appendChild(li);
+		inputField.value = '';
+	}
+
 }
 
 const handleRemoveTask = (event) => {
