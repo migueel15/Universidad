2b100f7c79477fc2b85a70252310a54681d47e6d
migueel15 <miguelangeldorado10@gmail.com>
2025-11-14 18:10:04 +0100

    Update Universidad: vie 14 nov 2025 18:10:04 CET

--- segundo/ADA/practicas/practica4/src/TarifaTelefonica.java
+++ segundo/ADA/practicas/practica4/src/TarifaTelefonica.java
@@ -2,55 +2,53 @@ import java.util.Arrays;
 
 public class TarifaTelefonica {
 
-	private int tarifaPlana, permanencia, tarifaMegas;
-	private int[] estimacion;
-	private int[] pago;// Pago mínimo a realizar durante los meses de i...n
+  private int tarifaPlana, permanencia, tarifaMegas;
+  private int[] estimacion;
+  private int[] pago; // Pago mínimo a realizar durante los meses de i...n
 
-	public TarifaTelefonica(int tp, int p, int tm, int[] est) {
-		tarifaPlana = tp;
-		permanencia = p;
-		tarifaMegas = tm;
-		estimacion = est;
-		pago = null;
-	}
+  public TarifaTelefonica(int tp, int p, int tm, int[] est) {
+    tarifaPlana = tp;
+    permanencia = p;
+    tarifaMegas = tm;
+    estimacion = est;
+    pago = null;
+  }
 
-	public int resolverBottomUp() {
-		int n = estimacion.length;
-		pago = new int[n+1];
-		pago[n] = 0;
+  public int resolverBottomUp() {
+    int n = estimacion.length;
+    pago = new int[n + 1];
+    pago[n] = 0;
 
-		for(int i = n-1; i >=0; i--){
-			pago[i] = tarifaMegas*estimacion[i] + pago[i+1];
+    for (int i = n - 1; i >= 0; i--) {
+      pago[i] = tarifaMegas * estimacion[i] + pago[i + 1];
 
-			if(i + permanencia <= n && pago[i] > permanencia*tarifaPlana + pago[i+permanencia]){
-				pago[i] = permanencia*tarifaPlana + pago[i+permanencia];
-			}
-		}
-		return pago[0];
-	}
+      if (i + permanencia <= n &&
+          pago[i] > permanencia * tarifaPlana + pago[i + permanencia]) {
+        pago[i] = permanencia * tarifaPlana + pago[i + permanencia];
+      }
+    }
+    return pago[0];
+  }
 
-	public int[] reconstruirSol() {
-		if (pago == null) {
-			throw new RuntimeException("Se debe resolver el problema primero");
-		}
-		int[] res = new int[estimacion.length];
-		for(int i = 0; i < estimacion.length; i++){
-			if(pago[i] == pago[1+i] + tarifaMegas*estimacion[i]){
-				res[i] = 0;
-			}else {
-				for(int j = 0; j < permanencia; j++){
-					res[i+j] = 1;
-				}
-				i+=permanencia-1;
-			}
-		}
-		return res;
-	}
+  public int[] reconstruirSol() {
+    if (pago == null) {
+      throw new RuntimeException("Se debe resolver el problema primero");
+    }
+    int[] res = new int[estimacion.length];
+    for (int i = 0; i < estimacion.length; i++) {
+      if (pago[i] == pago[1 + i] + tarifaMegas * estimacion[i]) {
+        res[i] = 0;
+      } else {
+        for (int j = 0; j < permanencia; j++) {
+          res[i + j] = 1;
+        }
+        i += permanencia - 1;
+      }
+    }
+    return res;
+  }
 
-	public void imprimeVectorSolucion() {
-		System.out.println(Arrays.toString(pago));
-	}
-
-
-
+  public void imprimeVectorSolucion() {
+    System.out.println(Arrays.toString(pago));
+  }
 }
