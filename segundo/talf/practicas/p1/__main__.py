85bdee39ea8c43cd27b06a74306fd502b329b1d7
migueel15 <miguelangeldorado10@gmail.com>
2026-03-12 17:32:18 +0100

    add p1 talf

---
+++ segundo/talf/practicas/p1/__main__.py
@@ -0,0 +1,17 @@
+from python.maths import power_relation
+from python.grammar import produce
+
+# Ejercicio 1
+# R = {(1, 1), (1, 2), (2, 3), (3, 4)}
+# R_cubed = power_relation(R, 3)
+
+# Ejercicio 3
+produce(
+    "ejercicio3",
+    num_strings=10,
+    max_derivation=20,
+    database_path="libs/python/grammar/grammars",
+)
+
+
+# Ejercicio 4
--- segundo/talf/practicas/p1/p1.py
+++
@@ -1,1 +0,0 @@
-from libs.python.maths import power_relation
